7a3a074161a6a408fa76cf846de3a35977a69d52
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-02-19 18:09:34 +0100

    Setup basic file structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>🌌 guidetothegalaxy.io</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/gianlucajahn/guidetothegalaxy',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discordapp.com/users/556559991062921217',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/gianlucajahn/guidetothegalaxy',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © 2023. The definitive guide to spacetravel on the web',
   },
 }
 
