ba2bc7d8071aee514004c821afe44f83c70efe89
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-03-11 22:19:50 +0100

    Add changes

--- components/text/guidetothegalaxy.tsx
+++ components/text/guidetothegalaxy.tsx
@@ -5,10 +5,7 @@ import styles from "./guidetothegalaxy.module.scss";
 export default function guidetothegalaxy() {
   return (
     <div className={styles.container}>
-      <Image src={require(".//../../resources/images/rocket.png")} alt="Rocket" className={`${styles.overlay} ${styles.rocket}`} />
-      <Image src={require(".//../../resources/images/ufo.png")} alt="UFO" className={`${styles.overlay} ${styles.ufo}`} />
-      <Image src={require(".//../../resources/images/planet.png")} alt="Planet" className={`${styles.overlay} ${styles.planet}`} />
-      <h1 className={styles.headline}>The Guide to the Galaxy</h1>
+      <h1 className={styles.headline}>The Guide to the Galaxy <span className={styles.book}>📖</span></h1>
     </div>
   );
 }
