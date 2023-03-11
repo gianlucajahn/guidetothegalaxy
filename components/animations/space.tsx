460e913c3d20687b1b9b3944b5a8f81a9f084879
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-03-11 17:20:31 +0100

    Add introduction chapter/page to project

---
+++ components/animations/space.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './space.module.scss';
+
+export default function Space () {
+  return (
+    <div className={styles.background}>
+        <div className={styles.space}>
+            
+        </div>
+    </div>
+  );
+}
