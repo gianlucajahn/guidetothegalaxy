11b3d8523da5ed033c810bbe7210ca14902c580c
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-03-30 19:51:35 +0200

    Remove unused package from next.config.js

--- next.config.js
+++ next.config.js
@@ -12,23 +12,6 @@ module.exports = withNextra({
   },
   webpack(config, options) {
     const { isServer } = options;
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/resources/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
-    });
 
     return config;
   },
