57106b8077d90791da4b8a851c3a29df367b61e3
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-04-22 15:06:51 +0200

    Fix proptypes in YoutubeEmbed.tsx

--- components/YoutubeEmbed/YoutubeEmbed.tsx
+++ components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
 import css from './YoutubeEmbed.module.scss';
 
-const YoutubeEmbed = ({ embedId }) => (
+type YoutubeEmbedProps = {
+    embedId: string;
+}
+
+const YoutubeEmbed = (props: YoutubeEmbedProps) => (
   <div className={css.videoResponsive}>
     <iframe
       width="853"
       height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      src={`https://www.youtube.com/embed/${props.embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
@@ -15,9 +18,5 @@ const YoutubeEmbed = ({ embedId }) => (
     />
   </div>
 );
-
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
 
 export default YoutubeEmbed;
