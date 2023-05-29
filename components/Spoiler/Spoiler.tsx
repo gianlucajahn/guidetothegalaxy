0f797583391ea6c170ce5ab7d3d9339ca9ebf3a2
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-05-29 21:25:02 +0200

    Format Spoiler.tsx

--- components/Spoiler/Spoiler.tsx
+++ components/Spoiler/Spoiler.tsx
@@ -7,22 +7,30 @@ interface SpoilerProps {
 }
 
 const expandedStyles = {
-    backgroundColor: "rgb(28, 118, 253)",
-    justifyContent: "flex-start",
-    padding: "14px 20px 20px 26px",
-    maxHeight: "500px",
-    flexDirection: "column"
+  backgroundColor: "rgb(28, 118, 253)",
+  justifyContent: "flex-start",
+  padding: "14px 20px 20px 26px",
+  maxHeight: "500px",
+  flexDirection: "column",
 };
 
 const foldedStyles = {
-    backgroundColor: "#393a3f",
+  backgroundColor: "#393a3f",
 };
 
 export default function Hero(props: SpoilerProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className={`${styles.spoiler} ${isExpanded ? styles.isExpanded : styles.isFolded}`} onClick={() => {!isExpanded ? setIsExpanded(!isExpanded) : null}} style={isExpanded ? expandedStyles : foldedStyles}>
+    <div
+      className={`${styles.spoiler} ${
+        isExpanded ? styles.isExpanded : styles.isFolded
+      }`}
+      onClick={() => {
+        !isExpanded ? setIsExpanded(!isExpanded) : null;
+      }}
+      style={isExpanded ? expandedStyles : foldedStyles}
+    >
       {isExpanded ? (
         <>
           <h2>👨‍🏫 Answer: {props.headline}</h2>
@@ -33,4 +41,4 @@ export default function Hero(props: SpoilerProps) {
       )}
     </div>
   );
-;}
+}
