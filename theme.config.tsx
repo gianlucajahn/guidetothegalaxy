ee88bf3b3f0bf3710727ef0fe46982cb460c27a0
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-05-31 01:49:14 +0200

    Change meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,10 +9,9 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
-      <title>The open-sourced guide on spacetravel | Guide To The Galaxy</title>
       <meta content="Guide To The Galaxy" property="og:site_name"></meta>
-      <meta property="og:title" content="The open-sourced guide on spacetravel | Guide To The Galaxy" />
-      <meta property="og:description" content="A thought-experiment through the future of mankind. Hand-crafted to inform and inspire." />
+      <meta property="og:description" content="A thought-experiment through the future of mankind. 
+      Hand-crafted to inform and inspire." />
       <meta property="og:image" content="https://camo.githubusercontent.com/3d8670c2ca3bbf10006321157a1adaa8f6f5bb8bcc9f9e735d32068b0b65c277/68747470733a2f2f692e696d6775722e636f6d2f51454a4632654a2e706e67" />
     </>
   ),
