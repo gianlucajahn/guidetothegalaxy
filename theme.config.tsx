5d72e72fa08ac58329e865c7627e2f58b607d66d
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-05-31 02:16:26 +0200

    Add twitter meta tags for embed to use big images

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,6 +14,11 @@ const config: DocsThemeConfig = {
       <meta name="theme-color" content="#1374FF"></meta>
       <meta property="og:description" content="A thought-experiment through the future of mankind. &#x0A; Hand-crafted to inform and inspire." />
       <meta property="og:image" content="https://camo.githubusercontent.com/3d8670c2ca3bbf10006321157a1adaa8f6f5bb8bcc9f9e735d32068b0b65c277/68747470733a2f2f692e696d6775722e636f6d2f51454a4632654a2e706e67" />
+      <meta name="twitter:site" content="Guide To The Galaxy"></meta>
+      <meta name="twitter:title" content="Guide To The Galaxy"></meta>
+      <meta name="twitter:description" content="A thought-experiment through the future of mankind. Hand-crafted to inform and inspire."></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:image:src" content="https://camo.githubusercontent.com/3d8670c2ca3bbf10006321157a1adaa8f6f5bb8bcc9f9e735d32068b0b65c277/68747470733a2f2f692e696d6775722e636f6d2f51454a4632654a2e706e67"></meta>
     </>
   ),
   project: {
