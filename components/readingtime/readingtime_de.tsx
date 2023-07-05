1ff2ff0c403161d088782ee7579c67833f16796f
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-07-05 20:11:29 +0200

    Add readingtime component to all german articles

---
+++ components/readingtime/readingtime_de.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from "next/router";
+import * as React from "react";
+import audiobookArray from "../../helpers/variables/audiobookArray";
+import styles from "./readingtime.module.scss";
+
+export interface ReadingTimeProps {}
+
+export default function ReadingTime(props: ReadingTimeProps) {
+  const router = useRouter();
+  const [audiobookDuration, setAudiobookDuration] = React.useState(undefined);
+  const [readTime, setReadTime] = React.useState(null);
+
+  React.useEffect(() => {
+    const path = router.pathname;
+
+    if (router.pathname.includes("motivation")) {
+      setAudiobookDuration(audiobookArray[0].languages[0].duration);
+    } else if (router.pathname.includes("destinations")) {
+      setAudiobookDuration(audiobookArray[1].languages[0].duration);
+    } else if (router.pathname.includes("introduction")) {
+      setAudiobookDuration(audiobookArray[2].languages[0].duration);
+    }
+
+    const readTimeVar = calcReadTime();
+    setReadTime(readTimeVar);
+  }, []);
+
+  const calcReadTime = () => {
+    if (typeof window === undefined) {
+      return;
+    }
+
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const paragraphs = document.getElementsByTagName("p");
+    let wordCount: number = 0;
+    for (let i = 0; i < 20; i++) {
+      let p = paragraphs[i];
+      if (p) {
+        let pContent = p.innerText;
+        wordCount += pContent.split(" ").length;
+      }
+    }
+    wordCount /= 130;
+    let readTime = Math.round(wordCount);
+    return readTime;
+  };
+
+  return (
+    <div className={styles.readingTime}>
+      <div className={styles.left}>
+        <div className={styles.line} />
+      </div>
+
+      <div className={styles.right}>
+        <p>Lesezeit: {readTime ? readTime : null} Minuten</p>
+        <p>Audiobook: In Arbeit ⚠️</p>
+      </div>
+    </div>
+  );
+}
