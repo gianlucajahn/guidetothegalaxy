ea32614a124fac4b419a9b5f2a93b971f5710abe
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-10-05 00:17:19 +0200

    Style account page

--- components/testFirebase/ArticleList.jsx
+++ components/testFirebase/ArticleList.jsx
@@ -1,18 +1,58 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "/firebase.js";
-
+import { auth } from "/firebase.js";
+import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import styles from "./firebase.module.css";
-
 import allArticles from "./allArticles";
+import { getDocSnap } from "../../helpers/firebaseFunctions";
 
 export default function ArticleList({ user }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user);
+      if (!loggedIn) setLoggedIn(true);
+      addNewUserDoc(user);
+    } else if (loggedIn) {
+      setCurrentUser({});
+      setLoggedIn(false);
+    }
+  });
+
+  async function addNewUserDoc(user) {
+    const docSnap = await getDocSnap(user.uid);
+    if (!docSnap.exists()) {
+      await setDoc(doc(db, "users", user.uid), {
+        email: user.email,
+        id: user.uid,
+        "read-articles": { index: true },
+      });
+    }
+  }
+
+  async function login() {
+    try {
+      const result = await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function logout() {
+    signOut(auth);
+  }
+
   const [userArticles, setUserArticles] = useState({});
 
   // executes given function on mount and every time the given document is updated in the database - the document is the logged in users document
   const unsub = onSnapshot(doc(db, "users", user.uid), (docSnap) => {
     // create a deep copy from the read-articles object in the users data
-    const readArticles = JSON.parse(JSON.stringify(docSnap.data()["read-articles"]));
+    const readArticles = JSON.parse(
+      JSON.stringify(docSnap.data()["read-articles"])
+    );
     setUserArticles(readArticles);
   });
 
@@ -20,12 +60,20 @@ export default function ArticleList({ user }) {
     return unsub;
   });
 
-  const articles = allArticles.map((articleName) => (
+  const articles = allArticles.map((article) => (
     <div className={styles.articlePrev}>
-      <p>{articleName}</p>
-      <div>{userArticles[articleName] ? "Article read" : "Not read"}</div>
+      <h2>{article.title}</h2>
+      <p>{article.desc}</p>
+      <div className={styles.status}>{userArticles[article.title] ? "Article read" : "Not read"}</div>
     </div>
   ));
 
-  return <div className={styles.articleList}>{articles}</div>;
+  return (
+    <div className={styles.articleList}>
+      <h1 className={styles.greeting}>
+        Hey, {currentUser.displayName?.substring(0, currentUser.displayName.indexOf(" "))}
+      </h1>
+      <div className={styles.array}>{articles}</div>
+    </div>
+  );
 }
--- components/testFirebase/allArticles.js
+++ components/testFirebase/allArticles.js
@@ -1,3 +1,20 @@
-const allArticles = ['introduction','mars','basecamp','rockets','raptor_engine']
+const allArticles = [
+    {
+        title: 'Introduction',
+        desc: 'Introduction Section to help users accomodate with the aerospace engineering niche'
+    },
+    {
+        title: 'Motivation',
+        desc: 'Motivation for Spaceflight and to colonize other planets to become multiplanetary'
+    },
+    {
+        title: 'Moon',
+        desc: 'Moon Flights and why we should do them - and which ones are already planned'
+    },
+    {
+        title: 'Mars',
+        desc: 'First trips to the moon and how we will survive on the marsian surface hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh hhhhh '
+    }
+];
 
 export default allArticles
